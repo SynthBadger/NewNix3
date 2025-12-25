941ed4a38fb645f23bf016262d99618812ae030c
SynthBadger <kmadrums@gmail.com>
2025-12-25 00:00:04 -0500

    niri maybe-4?

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        ./niri.nix
+
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
