30fd78f9f2f600ec9eac11aa1489bb3ce5263048
SynthBadger <kmadrums@gmail.com>
2025-12-25 21:19:18 -0500

    Going Back to ZSH/NH-2-optimize

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
+  description = "NixOS configuration for Imogen - 2025 Release";
+
   inputs = {
-    # Using the stable version
+    # NixOS 25.11 "Xantusia" is the stable release as of late 2025
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -14,24 +16,28 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-xivlauncher-rb, ... }: {
-    # NixOS configuration
-    nixosConfigurations.nixos =
-      nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.imogen = import ./home.nix;
-          }
-          nixos-xivlauncher-rb.nixosModules.default
-        ];
-      };
+  outputs = { self, nixpkgs, home-manager, nixos-xivlauncher-rb, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      # Pass 'inputs' to all modules so they can reference other flakes
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.imogen = import ./home.nix;
+          # Also pass 'inputs' to Home Manager specifically
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+        nixos-xivlauncher-rb.nixosModules.default
+      ];
+    };
 
-    # Package definitions using the provided pkgs argument
-        packages.x86_64-linux.niri = nixpkgs.lib.callPackage ./niri/default.nix { };
+    # Package definitions (exposed for 'nix build' or 'nh search')
+    packages.x86_64-linux = {
+      niri = nixpkgs.lib.callPackage ./niri/default.nix { };
+    };
   };
 }
