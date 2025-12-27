541220a24a7c3a8584fe0e8c905ca7fdff1804a8
SynthBadger <kmadrums@gmail.com>
2025-12-27 10:50:01 -0500

    HM Master Update-vesk-1

--- modules/audio.nix
+++ modules/audio.nix
@@ -9,8 +9,7 @@ services.pulseaudio.enable = false;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     };
 
