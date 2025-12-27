e8a0671995682a33f3bef46d73dde4e8535eaae6
SynthBadger <kmadrums@gmail.com>
2025-12-27 01:20:53 -0500

    Good Commit-opengl-erase

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,10 +1,9 @@
 
 
 { config, pkgs, ... }:
-
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       ../../modules/programs.nix
       ../../modules/audio.nix
@@ -23,16 +22,11 @@
     download-buffer-size = 1000000000;
   };
   networking.hostName = "nixos"; # Define your hostname.
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -45,34 +39,23 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.imogen = {
     isNormalUser = true;
     description = "imogen";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
+      thunderbird
     ];
   };
 
 
   programs.firefox.enable = true;
   programs.niri.enable = false;
-  #Steam Setup
 
-
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  hardware.opengl = {
-      enable = true;
-    };
+  hardware.graphics.enable = true;
 
   #Screen Sharing Stuff.
   xdg = {
@@ -85,31 +68,22 @@
       };
     };
 
-        environment.shells = with pkgs; [
+    programs.zsh.enable = true;
+    users.extraUsers.imogen = {
+    shell = pkgs.zsh;
+    };
+    environment.shells = with pkgs; [
        zsh
-      # fish
+     # fish
+       ];
 
-      ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  system.stateVersion = "25.05"; # Did you read the comment?
+    services.openssh.enable = true;
+    system.stateVersion = "25.05"; # Did you read the comment?
 
 }
