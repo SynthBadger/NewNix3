e7c9fac9b15eac5795ae48ff194a0c7d866cb083
SynthBadger <kmadrums@gmail.com>
2025-12-27 01:11:59 -0500

    Good Commit-OPEN GL-2

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -79,8 +79,8 @@
   portal = {
     enable = true;
       extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
+        #xdg-desktop-portal-kde
+        #xdg-desktop-portal-gtk
         ];
       };
     };
