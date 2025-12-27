ab5124dbaa774ef980a10852f79d74ceea4482e1
SynthBadger <kmadrums@gmail.com>
2025-12-27 11:24:14 -0500

    HM Master Update-vesk-bootparam-edit-2

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -10,8 +10,13 @@
   services.flatpak.enable = true;
   services.hardware.openrgb.enable = true;
 
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+    services.displayManager.sddm =
+    {
+      enable = true;
+      wayland.enable = true;
+    };
+
+ services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
